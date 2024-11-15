cd387cc177b3aea79a8983d9d8821466f0534b62
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-11-15 13:01:46 +0100

    Update auto calibration

--- src/calibration/auto_calibration.py
+++ src/calibration/auto_calibration.py
@@ -9,7 +9,7 @@ class AutoCalibration:
         """
         self.screen_size = screen_size
         self.calibration_data = []
-        self.is_calibrated = False
+        self._is_calibrated = False
 
     def collect_calibration_data(self, left_eye, right_eye):
         """
@@ -19,19 +19,15 @@ class AutoCalibration:
         :param right_eye: Coordonnées de l'œil droit
         """
         self.calibration_data.append((left_eye, right_eye))
-        
-        # Si un certain nombre de points de calibration est atteint, activer la calibration
-        if len(self.calibration_data) >= 10:  # Adaptez ce seuil selon les besoins
+    
+        if len(self.calibration_data) >= 10:  
             self.compute_calibration()
 
     def compute_calibration(self):
         """
         Effectue les calculs nécessaires pour la calibration.
         """
-        # Exemple de logique de calibration : moyenne des points de calibration
-        # Cela peut être amélioré avec des modèles plus avancés de mapping pour une précision accrue
-        self.is_calibrated = True
-        # Ajoutez ici vos calculs de calibration basés sur self.calibration_data
+        self._is_calibrated = True
         print("Calibration terminée avec succès.")
 
     def reset_calibration(self):
@@ -39,13 +35,14 @@ class AutoCalibration:
         Réinitialise l'état de la calibration et efface les données de calibration.
         """
         self.calibration_data = []
-        self.is_calibrated = False
+        self._is_calibrated = False
         print("Calibration réinitialisée.")
 
+    @property
     def is_calibrated(self):
         """
         Vérifie si la calibration a été effectuée avec succès.
         
         :return: Booléen indiquant si la calibration est terminée
         """
-        return self.is_calibrated
+        return self._is_calibrated
